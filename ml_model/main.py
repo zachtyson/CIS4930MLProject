f5cbb8f9d42646436125c0d5aefb756934337492
Zachary <tysonzachary@ufl.edu>
2024-02-17 01:55:31 -0500

    trains model to colorize and tests

--- ml_model/main.py
+++ ml_model/main.py
@@ -4,25 +4,28 @@ import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
 from torchvision import transforms
-
+from PIL import Image
+from torchvision.transforms.functional import to_pil_image
 from ImageColorizationDataset import ImageColorizationDataset
 
 
 class ColorizationNet(nn.Module):
     def __init__(self):
         super(ColorizationNet, self).__init__()
+        # Encoder
         self.encoder = nn.Sequential(
             nn.Conv2d(1, 64, kernel_size=3, stride=1, padding=1),
             nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            # Add more layers as needed
+            nn.MaxPool2d(kernel_size=2, stride=2),  # Output: 128x128
+            # You can add more layers here
         )
 
+        # Decoder
         self.decoder = nn.Sequential(
-            nn.ConvTranspose2d(64, 32, kernel_size=3, stride=2, padding=1, output_padding=1),  # Adjusted
+            nn.ConvTranspose2d(64, 32, kernel_size=3, stride=2, padding=1, output_padding=1),  # Output: 256x256
             nn.ReLU(inplace=True),
-            nn.ConvTranspose2d(32, 2, kernel_size=3, stride=2, padding=1, output_padding=1),  # Adjusted
-            # Add more layers as needed
+            nn.ConvTranspose2d(32, 3, kernel_size=3, stride=1, padding=1),  # Maintains 256x256 output
+            # Ensure output matches target size, adjust layers accordingly
         )
 
     def forward(self, x):
@@ -30,6 +33,12 @@ class ColorizationNet(nn.Module):
         x = self.decoder(x)
         return x
 
+
+def psnr(label, output, max_pixel=1.0):
+    mse = torch.mean((label - output) ** 2)
+    if mse == 0:
+        return 100
+    return 20 * torch.log10(max_pixel / torch.sqrt(mse))
 
 
 def main():
@@ -49,7 +58,7 @@ def main():
     criterion = nn.MSELoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
-    for epoch in range(1):
+    for epoch in range(10):
         running_loss = 0.0
         for i, data in enumerate(dataloader, 0):
             inputs, labels = data['black_image'], data['color_image']
@@ -59,11 +68,14 @@ def main():
             loss.backward()
             optimizer.step()
             running_loss += loss.item()
-            if i % 2000 == 1999:
+            if i % 1250 == 1249:
                 print(f'[{epoch + 1}, {i + 1}] loss: {running_loss / 2000}')
                 running_loss = 0.0
 
     print('Finished Training')
+
+    # load model colorization_model.pth
+    # model.load_state_dict(torch.load('colorization_model.pth'))
 
     # evaluate the model on the test set
     test_dataset = ImageColorizationDataset('data/test_black', 'data/test_color', transform=transform)
@@ -71,6 +83,14 @@ def main():
 
     correct = 0
     total = 0
+
+    # Example usage in your evaluation loop
+    with torch.no_grad():
+        for data in test_dataloader:
+            inputs, labels = data['black_image'], data['color_image']
+            outputs = model(inputs)
+            psnr_value = psnr(labels, outputs)
+            print(f'PSNR value: {psnr_value.item()} dB')
 
     # these are images so we need to compute the accuracy using MSE
     with torch.no_grad():
@@ -80,13 +100,40 @@ def main():
             total += (labels.size(0) * 256 * 256)
             correct += (torch.sum((outputs - labels) ** 2)).item()
 
-
-
     print(f'Accuracy of the network on the test images: {100 * correct / total}%')
 
     # save the model
     torch.save(model.state_dict(), 'colorization_model.pth')
 
+    # try to use model to colorize a black and white image
+    # open myphoto.jpg
+    # transform myphoto.jpg
+    # use model to colorize the image
+    # save the colorized image as myphoto_colorized.jpg
+
+    model = ColorizationNet()
+    model.load_state_dict(torch.load('colorization_model.pth'))
+    model.eval()
+
+    image_path = 'myphoto.jpg'
+    image = Image.open(image_path).convert('L')
+    transform = transforms.Compose([
+        transforms.Resize((256, 256)),
+        transforms.ToTensor(),
+    ])
+    input_image = transform(image).unsqueeze(0)  # Add batch dimension
+
+    # Colorize the image
+    with torch.no_grad():
+        output = model(input_image)
+
+    # Convert the output tensor to an image
+    output_image = output.squeeze().cpu().detach()
+    output_image = to_pil_image(output_image)
+    # denormalize the image
+
+
+    output_image.save('myphoto_colorized.jpg')
 
     # image colorization dataset, downloaded from kaggle
     # in the /data folder organized as follows:
