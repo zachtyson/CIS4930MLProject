4270911b08ff1a32061d05bc84564298675d33f2
Zachary <tysonzachary@ufl.edu>
2024-03-23 17:29:15 -0400

    service to upload file

--- frontend-colorization/src/app/image-colorization.service.ts
+++ frontend-colorization/src/app/image-colorization.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ImageColorizationService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
+
+  uploadFile(file: File) {
+    const formData = new FormData();
+    formData.append('file', file, file.name);
+
+    return this.http.post('http://localhost:8000/api/colorize', formData, {
+      headers: new HttpHeaders({
+        'Accept': 'application/json'
+      }),
+      responseType: 'json'
+    });
+  }
+
+
 }
