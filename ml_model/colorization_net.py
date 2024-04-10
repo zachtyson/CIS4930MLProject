82dc313ae51c7039fe08cd38a6743b852ab71311
Zachary <tysonzachary@ufl.edu>
2024-04-10 09:31:38 -0400

    change complexity of cnn

--- ml_model/colorization_net.py
+++ ml_model/colorization_net.py
@@ -1,6 +1,5 @@
 import torch.nn as nn
 
-
 class ColorizationNet(nn.Module):
     def __init__(self):
         super(ColorizationNet, self).__init__()
@@ -9,28 +8,40 @@ class ColorizationNet(nn.Module):
             # From my understanding, the input is a grayscale image, so the input channel is 1, goes into 32 channels
             # with 3x3 kernel, stride 1, padding 1 Honestly not really sure why I went to 128 but I'm just
             # experimenting with random numbers
-            nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(1, 32, kernel_size=5, stride=1, padding=2),
             nn.ReLU(inplace=True),
             nn.BatchNorm2d(32),
-            nn.Conv2d(32, 64, kernel_size=3, stride=2, padding=1),
+            # Changed kernel to 5 just to test it
+            nn.Conv2d(32, 64, kernel_size=5, stride=2, padding=2),
             nn.ReLU(inplace=True),
             nn.BatchNorm2d(64),
-            # Reduce depth and channel size slightly
-            nn.Conv2d(64, 128, kernel_size=3, stride=2, padding=1),
+            nn.Conv2d(64, 128, kernel_size=5, stride=2, padding=2),
             nn.ReLU(inplace=True),
             nn.BatchNorm2d(128),
+            nn.Conv2d(128, 256, kernel_size=5, stride=2, padding=2),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(256),
+            nn.Conv2d(256, 512, kernel_size=5, stride=2, padding=2),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(512),
         )
 
         # Decoder
         self.decoder = nn.Sequential(
-            nn.ConvTranspose2d(128, 64, kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.ConvTranspose2d(512, 256, kernel_size=5, stride=2, padding=2, output_padding=1),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(256),
+            nn.ConvTranspose2d(256, 128, kernel_size=5, stride=2, padding=2, output_padding=1),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(128),
+            nn.ConvTranspose2d(128, 64, kernel_size=5, stride=2, padding=2, output_padding=1),
             nn.ReLU(inplace=True),
             nn.BatchNorm2d(64),
-            # Simplifying the upscaling process
-            nn.ConvTranspose2d(64, 32, kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.ConvTranspose2d(64, 32, kernel_size=5, stride=2, padding=2, output_padding=1),
             nn.ReLU(inplace=True),
             nn.BatchNorm2d(32),
-            nn.Conv2d(32, 3, kernel_size=3, stride=1, padding=1),
+            # The final convolution to 3 channels for RGB output. Maintaining kernel size 5 and adjusting padding.
+            nn.Conv2d(32, 3, kernel_size=5, stride=1, padding=2),
         )
 
     def forward(self, x):
