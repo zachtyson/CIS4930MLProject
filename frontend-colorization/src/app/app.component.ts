41fc4a1db490d48699d794b02cbbc0a82a6792e7
Zachary <tysonzachary@ufl.edu>
2024-04-20 16:48:58 -0400

    expand for image

--- frontend-colorization/src/app/app.component.ts
+++ frontend-colorization/src/app/app.component.ts
@@ -7,62 +7,80 @@ import { ImageColorizationService } from "./image-colorization.service";
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  selectedFile: File | null = null;
-  originalImageSrc: string = '';
-  imageSrc: string = '';
+  selectedFiles: File[] = [];
   successMessage: string = '';
   errorMessage: string = '';
+  images: { original: string, processed?: string, expanded?: boolean }[] = [];
+
+  toggleImage(index: number): void {
+    this.images[index].expanded = !this.images[index].expanded;
+  }
+
+  removeImage(index: number): void {
+    this.images.splice(index, 1); // Removes the image from the array
+  }
 
   constructor(private fileUploadService: ImageColorizationService) { }
 
   onFileSelected(event: any) {
-    const file: File = event.target.files[0];
+    const files: FileList = event.target.files;
+    this.processFiles(files);
+  }
 
-    if (file) {
-      // Check if the file is a JPEG/JPG.
+  onFilesDropped(event: DragEvent) {
+    event.preventDefault();
+    if (event.dataTransfer && event.dataTransfer.files) {
+      const files: FileList = event.dataTransfer.files;
+      this.processFiles(files);
+    }
+  }
+
+  onDragOver(event: Event) {
+    event.preventDefault();
+  }
+
+  onDragLeave(event: Event) {
+    event.preventDefault();
+  }
+
+  processFiles(files: FileList) {
+    for (let i = 0; i < files.length; i++) {
+      const file = files[i];
       if (file.type.match('image/jpeg')) {
-        this.selectedFile = file;
-        // Convert original image to base64.
         const reader = new FileReader();
         reader.onload = (e: any) => {
-          this.originalImageSrc = e.target.result; // The Base64 string.
+          this.images.push({original: e.target.result});
         };
-        reader.readAsDataURL(this.selectedFile);
-        this.errorMessage = '';
+        reader.readAsDataURL(file);
+        this.selectedFiles.push(file);
       } else {
         this.errorMessage = 'Only JPEG/JPG files are allowed.';
-        this.selectedFile = null;
       }
-    } else {
-      this.errorMessage = 'Please select a file.';
-      this.selectedFile = null;
-      this.successMessage = '';
     }
-    this.imageSrc = '';
-    this.successMessage = '';
-    this.originalImageSrc = '';
-
   }
 
   onUpload() {
-    if (!this.selectedFile) {
+    if (!this.selectedFiles.length) {
       this.errorMessage = 'Please select a file to upload.';
       return;
     }
-    this.fileUploadService.uploadFile(this.selectedFile).subscribe(response => {
-      const i: ImageColorizationResponse = response as ImageColorizationResponse;
-      this.successMessage = 'Upload success';
-      this.errorMessage = ''; // Clear any previous error messages
-      if (i.image) {
-        this.imageSrc = 'data:image/jpeg;base64,' + i.image;
-      }
-    }, error => {
-      this.errorMessage = 'Upload error: ' + error.message;
-      this.successMessage = ''; // Clear any previous success messages
-    });
+    for (let j = 0; j < this.selectedFiles.length; j++) {
+      this.fileUploadService.uploadFile(this.selectedFiles[j]).subscribe(response => {
+        const i: ImageColorizationResponse = response as ImageColorizationResponse;
+        this.successMessage = 'Upload success';
+        this.errorMessage = ''; // Clear any previous error messages
+        if (i.image) {
+          this.images[j].processed = 'data:image/jpeg;base64,' + i.image;
+        }
+      }, error => {
+        this.errorMessage = 'Upload error: ' + error.message;
+        this.successMessage = ''; // Clear any previous success messages
+      });
+    }
   }
 }
 
 interface ImageColorizationResponse {
   image: string;
 }
+
